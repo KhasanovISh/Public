b86d6a7977e204ca4d3c804b88046141772f813e
KhasanovISh <khasanov@ntcup.ru>
2023-05-05 14:05:37 +0300

    initial template files

---
+++ 2023/Photonics/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-05-05 14:00:04
+@author: I. Khasanov
+"""
+
+from model import *
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ 2023/Photonics/model.py
@@ -0,0 +1,15 @@
+# local imports
+import sys
+sys.path.append('E:\Python\Common\SPPPy')
+from SPPPy import ExperimentSPR, Layer, MaterialDispersion
+sys.path.append('E:\Python\Common')
+from optics import *
+from helper import *
+from settings import *
+
+# general imports
+from pathlib import Path
+
+import numpy as np
+from numpy.lib import scimath as SM
+import matplotlib.pyplot as plt
